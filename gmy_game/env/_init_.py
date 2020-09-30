f66b59756f430cd627dc1f61bcae8abff32e9482
Sara-Klein <71388804+Sara-Klein@users.noreply.github.com>
2020-09-30 16:22:50 +0200

    ein paar veraenderungen

--- gmy_game/env/_init_.py
+++ gmy_game/env/_init_.py
@@ -1,8 +1,2 @@
-from gym.envs.registration import register
-
-regsiter(
-  id = 'Pygame-v0',
-  entry_point = 'gmy_game.envs:CustomEnv',
-  max_episode_steps = 2000,
-  )
-  
+from gym_game.envs.custom_env import *
+from gym_game.envs.pygame_2d import PyGame2D
